3617cf7aaa6117c78942c631d2acc7a53e9454f8
Omaralsaabi <prof.omaralsaabi@gmail.com>
2024-01-02 00:41:51 +0300

    TokenaizerV2

--- preprocessing.py
+++ preprocessing.py
@@ -32,7 +32,6 @@ print(vocab_size)
 
 vocab = {token:integer for integer,token in enumerate(all_words)}
 for i, item in enumerate(vocab.items()):
-    print(item)
     if i >= 50:
         break
 
@@ -61,3 +60,33 @@ ids = tokenizer.encode(text)
 print(ids)
 words= tokenizer.decode(ids)
 print(words)
+
+all_words.extend(["<|endoftext|>", "<|UNK|>"])
+
+vocab = {token:integer for integer,token in enumerate(all_words)}
+
+for i, item in enumerate(list(vocab.items())[-5:]):
+    print(item)
+
+class SimpleTokenizerV2:
+    def __init__(self, vocab):
+        self.str_to_int = vocab
+        self.int_to_str = { i:s for s,i in vocab.items()}
+    def encode(self, text):
+        preprocessed = re.split(r'([,.?_!"()\']|--|\s)', text)
+        preprocessed = [item.strip() for item in preprocessed if item.strip()]
+        preprocessed = [item if item in self.str_to_int   #A
+        else "<|UNK|>" for item in preprocessed]
+        ids = [self.str_to_int[s] for s in preprocessed]
+        return ids
+    def decode(self, ids):
+        text = " ".join([self.int_to_str[i] for i in ids])
+        text = re.sub(r'\s+([,.?!"()\'])', r'\1', text) #B
+        return text
+    
+text1 = "الحمد لله رب العالمين والصلاة والسلام على محمد  وعلى آله"
+text2 = "أنا اسمي عمر"
+text = " <|endoftext|> ".join([text1, text2])
+
+tokenizer = SimpleTokenizerV2(vocab)
+print(tokenizer.decode(tokenizer.encode(text)))
